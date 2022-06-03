d0b6fe894c9033ba5b716c0b6d7712204d09c478
Antanas Pužas <antanaspuzas@protonmail.com>
2022-06-03 17:28:51 +0300

    Add game function that plays the game for 5 rounds. Also add some wrong input handling

--- script.js
+++ script.js
@@ -11,25 +11,43 @@ let playRound = (playerSelection, computerSelection) => {
                 return "You Win! Paper beats Rock!";
             } else if (playerSelection === "scissors") {
                 return "You Lose! Rock beats Scissors!";
-            } else {
+            } else if (playerSelection === "rock") {
                 return "It's a tie! Go again!";
+            } else {
+                return "Error?";
             }
         case "Scissors":
             if (playerSelection === "paper") {
                 return "You Lose! Scissors beats Paper!";
             } else if (playerSelection === "scissors") {
                 return "It's a tie! Go again!";
-            } else {
+            } else if (playerSelection === "rock") {
                 return "You Win! Rock beats Scissors!";
+            } else {
+                return "Error?";
             }
         case "Paper":
             if (playerSelection === "paper") {
                 return "It's a tie! Go again!"
             } else if (playerSelection === "scissors") {
                 return "You Win! Scissors beats Paper!";
-            } else {
+            } else if (playerSelection === "rock") {
                 return "You Lose! Paper beats Rock!";
+            } else {
+                return "Error?";
             }
     }
 }
 
+let game = () => {
+    let playerSelection;
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Rock, Paper or Scissors?");
+        computerSelection = computerPlay();
+        console.log("Computer chooses " + computerSelection + "!");
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    console.log("Game Over!");
+}
+
+game();
