cc185c971d4d8d1187b122866f086a8c544b415f
Antanas Pužas <antanaspuzas@protonmail.com>
2022-06-03 18:32:01 +0300

    Remove game function and implement game logic to playRound()

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ let computerPlay = () => {
 }
 
 let possiblePlayerSelection = ["rock", "paper", "scissors"];
+let winningConditions = [{ player: "rock", computer: "scissors" }, { player: "scissors", computer: "paper" },
+{ player: "paper", computer: "rock" }];
 
 let playRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toLowerCase();
-    if (possiblePlayerSelection.indexOf(playerSelection) === -1) {
-        return "Error";
+    let choices = { player: playerSelection.toLowerCase(), computer: computerSelection.toLowerCase() };
+    let result = -1;
+    if (possiblePlayerSelection.indexOf(choices.player) === -1) {
+        result = "Error";
     }
-}
 
-let playerSelection = "paper";
-let computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+    if (choices.player === choices.computer) {
+        result = 0;
+    } 
+
+    for (let i = 0; i < winningConditions.length; i++) {
+        if (JSON.stringify(choices) === JSON.stringify(winningConditions[i])) {
+            result = 1
+        }
+    }
+    return result;
+}
