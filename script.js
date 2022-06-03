dbe16b04773984d89b4f1910a4e6c8ef6d4b51b2
Antanas Pužas <antanaspuzas@protonmail.com>
2022-06-03 17:20:33 +0300

    Add playRound function that outputs the outcome of one round

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 let computerPlay = () => {
     let random = Math.floor(Math.random() * 3) + 1;
-    return random === 1 ? "Rock" : random === 2 ? "Paper" : "Scissors"; 
+    return random === 1 ? "Rock" : random === 2 ? "Paper" : "Scissors";
 }
+
+let playRound = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+    switch (computerSelection) {
+        case "Rock":
+            if (playerSelection === "paper") {
+                return "You Win! Paper beats Rock!";
+            } else if (playerSelection === "scissors") {
+                return "You Lose! Rock beats Scissors!";
+            } else {
+                return "It's a tie! Go again!";
+            }
+        case "Scissors":
+            if (playerSelection === "paper") {
+                return "You Lose! Scissors beats Paper!";
+            } else if (playerSelection === "scissors") {
+                return "It's a tie! Go again!";
+            } else {
+                return "You Win! Rock beats Scissors!";
+            }
+        case "Paper":
+            if (playerSelection === "paper") {
+                return "It's a tie! Go again!"
+            } else if (playerSelection === "scissors") {
+                return "You Win! Scissors beats Paper!";
+            } else {
+                return "You Lose! Paper beats Rock!";
+            }
+    }
+}
+
