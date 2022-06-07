3c5e46917070b57b8d1af26d9b5fb0c3b0b24fdf
Antanas Pužas <antanaspuzas@protonmail.com>
2022-06-07 16:36:49 +0300

    Refactor output of game and call game() instead of playRound() on button presses

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const possiblePlayerSelection = ["rock", "paper", "scissors"];
 const winningConditions = [{ player: "rock", computer: "scissors" }, { player: "scissors", computer: "paper" },
 { player: "paper", computer: "rock" }];
 
-
 // Plays a round and returns 0 for draw, 1 for player win and -1 for player lose
 let playRound = (playerSelection, computerSelection) => {
     let choices = { player: playerSelection.toLowerCase(), computer: computerSelection.toLowerCase() };
@@ -33,27 +32,30 @@ const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
 
 const resultsDiv = document.querySelector("#results");
-const resultsText = document.querySelector("#results-text")
 
-rockButton.addEventListener("click", () => resultsDiv.textContent = playRound("rock", computerPlay()));
-paperButton.addEventListener("click", () => resultsDiv.textContent = playRound("paper", computerPlay()));
-scissorsButton.addEventListener("click", () => resultsDiv.textContent = playRound("scissors", computerPlay()));
+rockButton.addEventListener("click", () => resultsDiv.innerHTML =
+    game(rockButton.id));
+paperButton.addEventListener("click", () => resultsDiv.innerHTML = 
+    game(paperButton.id));
+scissorsButton.addEventListener("click", () => resultsDiv.innerHTML = 
+    game(scissorsButton.id));
 
-// let game = () => {
-//     let computerSelection = computerPlay();
-//     let playerSelection = prompt("Please input Rock, Paper or Scissors:");
-//     let playerSelectionFormatted = playerSelection.charAt(0).toUpperCase() +
-//         playerSelection.slice(1).toLowerCase();
-//     let roundOutcome = playRound(playerSelection, computerSelection);
+function resultString(roundOutcome, playerSelection, computerSelection) {
+    let result = "Computer selection: " + computerSelection + "<br>"
+        + "Player selection: " + playerSelection + "<br>";
 
-//     console.log("Computer selection: " + computerSelection + "\n"
-//         + "Player selection: " + playerSelectionFormatted + "\n" +
-//         (roundOutcome === "Error" ? "Error"
-//             : roundOutcome === 0 ? "It's a Draw! Go Again!"
-//                 : roundOutcome === -1 ? "You Lose! " + computerSelection +
-//                     " beats " + playerSelectionFormatted + "!"
-//                     : "You Win! " + playerSelectionFormatted + " beats " + computerSelection + "!"));
-//     console.log("Game Over!");
-// }
+    roundOutcome === "Error" ? result += "Error"
+        : roundOutcome === 0 ? result += "It's a Draw! Go Again!"
+            : roundOutcome === -1 ? result += "You Lose! " + computerSelection +
+                " beats " + playerSelection + "!"
+                : result += "You Win! " + playerSelection + " beats " + computerSelection + "!";
+    return result;
+}
 
-// game();
+let game = (playerSelection) => {
+    let computerSelection = computerPlay();
+    let roundOutcome = playRound(playerSelection, computerSelection);
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+
+    return resultString(roundOutcome, playerSelection, computerSelection);
+}
